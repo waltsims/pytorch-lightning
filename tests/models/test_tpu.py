0ddbb2fd014e386fc8d76b5fb828af558d39932d
William Falcon <waf2107@columbia.edu>
2020-07-11 13:48:51 -0400

    remove grad scaling tpu

--- tests/models/test_tpu.py
+++ tests/models/test_tpu.py
@@ -32,8 +32,8 @@ def test_base_tpu_model_1(tmpdir):
         progress_bar_refresh_rate=0,
         max_epochs=1,
         tpu_cores=1,
-        limit_train_batches=0.4,
-        limit_val_batches=0.4
+        limit_train_batches=10,
+        limit_val_batches=10,
     )
 
     model = EvalModelTemplate()
@@ -49,8 +49,8 @@ def test_base_tpu_model_idx_1(tmpdir):
         progress_bar_refresh_rate=0,
         max_epochs=1,
         tpu_cores=[1],
-        limit_train_batches=0.4,
-        limit_val_batches=0.4
+        limit_train_batches=10,
+        limit_val_batches=10,
     )
 
     model = EvalModelTemplate()
@@ -66,8 +66,8 @@ def test_base_tpu_model_8(tmpdir):
         progress_bar_refresh_rate=0,
         max_epochs=1,
         tpu_cores=8,
-        limit_train_batches=0.4,
-        limit_val_batches=0.4
+        limit_train_batches=10,
+        limit_val_batches=10,
     )
 
     model = EvalModelTemplate()
@@ -92,8 +92,8 @@ def test_base_tpu_16bit_model_core_1(tmpdir):
         progress_bar_refresh_rate=0,
         max_epochs=1,
         tpu_cores=1,
-        limit_train_batches=0.4,
-        limit_val_batches=0.4
+        limit_train_batches=10,
+        limit_val_batches=10,
     )
 
     model = EvalModelTemplate()
@@ -111,8 +111,8 @@ def test_base_tpu_16bit_model_idx_core(tmpdir):
         progress_bar_refresh_rate=0,
         max_epochs=1,
         tpu_cores=[1],
-        limit_train_batches=0.4,
-        limit_val_batches=0.4
+        limit_train_batches=10,
+        limit_val_batches=10,
     )
 
     model = EvalModelTemplate()
@@ -130,8 +130,8 @@ def test_base_tpu_16bit_model_8_cores(tmpdir):
         progress_bar_refresh_rate=0,
         max_epochs=1,
         tpu_cores=8,
-        limit_train_batches=0.4,
-        limit_val_batches=0.4
+        limit_train_batches=10,
+        limit_val_batches=10,
     )
 
     model = EvalModelTemplate()
@@ -192,8 +192,8 @@ def test_single_tpu_core_model(tmpdir):
         default_root_dir=tmpdir,
         progress_bar_refresh_rate=0,
         max_epochs=1,
-        train_percent_check=0.1,
-        val_percent_check=0.1,
+        limit_train_batches=10,
+        limit_val_batches=10,
         tpu_cores=8,
     )
     trainer.fit(model)
@@ -208,8 +208,8 @@ def test_multi_core_tpu_model(tmpdir):
     trainer = Trainer(
         default_root_dir=tmpdir,
         max_epochs=1,
-        train_percent_check=0.4,
-        val_percent_check=0.2,
+        limit_train_batches=10,
+        limit_val_batches=10,
         tpu_cores=[1, 8],
     )
     trainer.fit(model)
@@ -254,8 +254,8 @@ def test_exception_when_no_tpu_found(tmpdir):
     trainer = Trainer(
         default_root_dir=tmpdir,
         max_epochs=1,
-        train_percent_check=0.4,
-        val_percent_check=0.2,
+        limit_train_batches=10,
+        limit_val_batches=10,
         tpu_cores=8,
     )
 
